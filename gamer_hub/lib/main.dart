81b0c0679bee332af8cc7e1c5297e7cde87c0458
Eren Satoğlu <satoglu24@gmail.com>
2022-06-16 02:58:35 +0300

    Commit

--- gamer_hub/lib/main.dart
+++ gamer_hub/lib/main.dart
@@ -1,28 +1,25 @@
 import 'package:flutter/material.dart';
+import 'package:gamer_hub/models/user_instance.dart';
 import 'package:gamer_hub/route_generator.dart';
 import 'package:gamer_hub/splash.dart';
 
-
-
-void main() {
+Future<void> main() async {
   runApp(const GamerHub());
 }
 
 class GamerHub extends StatefulWidget {
   const GamerHub({Key? key}) : super(key: key);
-  
+
   @override
   State<GamerHub> createState() => _GamerHubState();
 }
-  
+
 class _GamerHubState extends State<GamerHub> {
   @override
   Widget build(BuildContext context) {
     return const MaterialApp(
-      onGenerateRoute: RouteGenerator.routeGenerator,
-      title: "GamerHub App",
-      home: SplashScreen()
-
-    );
+        onGenerateRoute: RouteGenerator.routeGenerator,
+        title: "GamerHub App",
+        home: SplashScreen());
   }
 }
